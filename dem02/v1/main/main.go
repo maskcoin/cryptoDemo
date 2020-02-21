ec8b6503508b88d4ecd687f6cd4ff710d6d1b016
maskcoin <xuchanghui888@gmail.com>
2020-02-21 15:09:20 +0800

    创建区块链结构及创世块

--- dem02/v1/main/main.go
+++ dem02/v1/main/main.go
@@ -29,9 +29,28 @@ func NewBlock(data, prevBlockHash []byte) *Block {
 	return block
 }
 
+// 引入区块链
+type BlockChain struct {
+	//定义一个区块链切片
+	blocks []*Block
+}
+
+func GenesisBlock() *Block {
+	return NewBlock([]byte("Genesis block"), nil)
+}
+
+func NewBlockChain() *BlockChain {
+	genesisBlock := GenesisBlock()
+
+	return &BlockChain{blocks:[]*Block{genesisBlock}}
+}
+
 func main() {
-	block := NewBlock([]byte("老师转班长一枚比特币！"), nil)
-	fmt.Printf("前区块哈希值：%x\n", block.PrevHash)
-	fmt.Printf("当前区块哈希值：%x\n", block.Hash)
-	fmt.Printf("数据：%s\n", block.Data)
+	bc := NewBlockChain()
+	for i, block := range bc.blocks{
+		fmt.Printf("========= 当前区块高度:%d ==========\n", i)
+		fmt.Printf("前区块哈希值：%x\n", block.PrevHash)
+		fmt.Printf("当前区块哈希值：%x\n", block.Hash)
+		fmt.Printf("数据：%s\n", block.Data)
+	}
 }
